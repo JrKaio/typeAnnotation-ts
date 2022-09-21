cc6cb898e23adcdb18f356e2895cebabe78a4e9a
Kaio Júnior <100144310+JrKaio@users.noreply.github.com>
2022-09-20 21:03:54 -0300

    Add files via upload

---
+++ typeAnnotation.js
@@ -0,0 +1,14 @@
+"use strict";
+//[TYPE ANNOTATION]
+let nome = 'Kaio';
+console.log(nome);
+let animais = ['elefante', 'gato', 'cavalo'];
+console.log(animais);
+//Objetos [TYPE ANNOTATION]
+let carro;
+carro = { nome: 'Palio', ano: 2020, preco: 12.000 };
+console.log(carro);
+function mult(num1, num2) {
+    return num1 * num2;
+}
+console.log(mult(12, 15));
